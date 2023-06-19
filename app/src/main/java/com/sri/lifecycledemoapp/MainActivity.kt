c09d1cc5952ce4255b79e4491a5e4249af585043
Sai karthik <saikarthiksistla@gmail.com>
2023-06-19 21:08:04 +0530

    counter app logic done

--- app/src/main/java/com/sri/lifecycledemoapp/MainActivity.kt
+++ app/src/main/java/com/sri/lifecycledemoapp/MainActivity.kt
@@ -3,20 +3,42 @@ package com.sri.lifecycledemoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private var counter: Int =0
+    private lateinit var counterTextView:TextView
+    private lateinit var addButton:FloatingActionButton
+    private lateinit var subtractButton:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("Demo","MainActivity onCreate called")
         setContentView(R.layout.activity_main)
+        counterTextView=findViewById(R.id.counter_textview)
+        addButton=findViewById(R.id.addBtn)
+        subtractButton=findViewById(R.id.subtractBtn)
+        addButton.setOnClickListener {
+            incrementCounter()
+        }
+        subtractButton.setOnClickListener {
+            decrementCounter()
+        }
+
+
     }
 
 
     override fun onStart() {
         super.onStart()
         Log.i("Demo","MainActivity onStart called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Demo","MainActivity onRestart called")
     }
 
     override fun onResume() {
@@ -39,12 +61,30 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("Demo","MainActivity onSaveInstanceState called")
+        outState.putInt("count",counter)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("Demo","MainActivity onRestoreInstanceState called")
+        counter=savedInstanceState.getInt("count")
+        Log.i("Demo","MainActivity: value of counter=$counter")
+        counterTextView.text=counter.toString()
+        super.onRestoreInstanceState(savedInstanceState)
+    }
 
 
-/**
-  For incrementing the counter
- **/
+
     fun incrementCounter(){
         counter++
+        counterTextView.text=counter.toString()
+    }
+
+
+    fun decrementCounter(){
+        counter--
+        counterTextView.text=counter.toString()
     }
 }
