7ce2811a85ae6cd0fdfb614b435a12ebc7833a35
Sai karthik <saikarthiksistla@gmail.com>
2023-06-20 23:03:10 +0530

    some callbacks added for a fragment

--- app/src/main/java/com/sri/lifecycledemoapp/fragment_demo/DemoFragment.kt
+++ app/src/main/java/com/sri/lifecycledemoapp/fragment_demo/DemoFragment.kt
@@ -1,6 +1,7 @@
 package com.sri.lifecycledemoapp.fragment_demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import com.sri.lifecycledemoapp.R
 
 class DemoFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("Demo","DemoFragment onCreate called")
         super.onCreate(savedInstanceState)
     }
 
@@ -17,44 +19,54 @@ class DemoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("Demo","DemoFragment onCreateView called")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_demo, container, false)
+
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i("Demo","DemoFragment onViewCreated called")
         super.onViewCreated(view, savedInstanceState)
         // Fragment's onViewCreated logic
     }
 
     override fun onStart() {
+        Log.i("Demo","DemoFragment onStart called")
         super.onStart()
         // Fragment's onStart logic
     }
 
     override fun onResume() {
+        Log.i("Demo","DemoFragment onResume called")
         super.onResume()
         // Fragment's onResume logic
     }
 
     override fun onPause() {
+        Log.i("Demo","DemoFragment onPause called")
         super.onPause()
         // Fragment's onPause logic
     }
 
     override fun onStop() {
+        Log.i("Demo","DemoFragment onStop called")
         super.onStop()
         // Fragment's onStop logic
     }
 
     override fun onDestroyView() {
+        Log.i("Demo","DemoFragment onDestroyView called")
         super.onDestroyView()
         // Fragment's onDestroyView logic
     }
     override fun onDestroy() {
+        Log.i("Demo","DemoFragment onDestroy called")
         super.onDestroy()
         // Fragment's onDestroy logic
     }
 
     override fun onDetach() {
+        Log.i("Demo","DemoFragment onDetach called")
         super.onDetach()
         // Fragment's onDetach logic
     }
--- app/src/main/java/com/sri/lifecycledemoapp/fragment_demo/SecondActivity.kt
+++ app/src/main/java/com/sri/lifecycledemoapp/fragment_demo/SecondActivity.kt
@@ -2,11 +2,51 @@ package com.sri.lifecycledemoapp.fragment_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.sri.lifecycledemoapp.R
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("Demo","SecondActivity onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Demo","SecondActivity onStart called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Demo","SecondActivity onRestart called")
+    }
+
+    override fun onResume() {
+        Log.i("Demo","SecondActivity onResume called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("Demo","SecondActivity onPause called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("Demo","SecondActivity onStop called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("Demo","SecondActivity onDestroy called")
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("Demo","SecondActivity onSaveInstanceState called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("Demo","SecondActivity onRestoreInstanceState called")
     }
 }
